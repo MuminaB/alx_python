2af79f4fe3b248b2c8e6464f8adf03e3d460e220
MuminaB <barrowmuminatou@gmail.com>
2023-11-27 13:41:01 +0000

    space

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,9 +10,9 @@ else:
     lastNumber = number % 10
 
 if lastNumber > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5".format(number, lastNumber), end="")
+    print("Last digit of {:d} is {:d} and is greater than 5 ".format(number, lastNumber), end="")
 elif lastNumber < 6 and number % 10 != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, lastNumber), end="")
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0 ".format(number, lastNumber), end="")
 else:
     print("Last digit of {:d} is {:d} and is 0".format(number, lastNumber))
       
