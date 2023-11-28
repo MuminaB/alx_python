5ef8303e8362eae81202be780fcfcf0a034067fa
MuminaB <barrowmuminatou@gmail.com>
2023-11-28 12:32:00 +0000

    update

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,10 +2,10 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    number = number * 1
+    number = number * -1
     lastNumber = number % 10
     if lastNumber != 0:
-        lastNumber = lastNumber * -1
+        lastNumber = lastNumber * 1
 else:
     lastNumber = number % 10
 
