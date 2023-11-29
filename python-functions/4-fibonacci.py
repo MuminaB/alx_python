cb35d17e49988dac89d2ab99aa72a67be33cfff3
MuminaB <barrowmuminatou@gmail.com>
2023-11-29 16:19:12 +0000

    Fibonacci

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,2 +1,7 @@
 def fibonacci_sequence(n):
-    return 
+    if n < 0 or n == 0:
+        print("")
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci_sequence(n-1) + fibonacci_sequence(n-2)
