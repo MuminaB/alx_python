95484ddce566c03eda5dafd574d107287d2499c1
MuminaB <barrowmuminatou@gmail.com>
2023-11-30 19:50:04 +0000

    update

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -8,10 +8,9 @@ def validate_password(password):
         return not validate
     if not any(char.isdigit()for char in password):
         return not validate
-    if not any(char.isspace()for char in password):
+    if any(char.isspace()for char in password):
         return not validate
-    if not validate:
-        return False
-    else: 
+    if validate:
         return True
-       
+    else: 
+        return False
