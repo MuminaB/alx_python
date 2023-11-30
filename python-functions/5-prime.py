bbf94074e84cb3003eb443f657c76b69536839b0
MuminaB <barrowmuminatou@gmail.com>
2023-11-30 19:29:20 +0000

    prime number update

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,9 +1,12 @@
 def is_prime(number):
-    
-    
-    if number % 2 == 1:
+    if number <= 1:
+        return False
+    primeNumber = True
+    if primeNumber:
         return True
     else:
         return False
-     
+    for i in range(2, number):
+        if number % i == 0:
+            return False
            
