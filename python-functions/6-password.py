952e87068cc83f5a8a17684414daf294120ff501
MuminaB <barrowmuminatou@gmail.com>
2023-11-30 20:20:53 +0000

    password

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -14,9 +14,4 @@ def validate_password(password):
         return True
     else: 
         return False
-    
-    
-print(validate_password("Password123"))
-print(validate_password("abc123"))
-print(validate_password("Password 123"))
-print(validate_password("password123"))
+      
