435996b78046927beb6b56973d2dd9139407f8c3
MuminaB <barrowmuminatou@gmail.com>
2023-12-04 19:41:53 +0000

    Multiple return

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,11 @@
+def multiple_returns(sentence):
+    if(len(sentence) > 0):
+        return((len(sentence), sentence[0]))
+    return((len(sentence), None))
+
+#def multiple_returns(sentence):
+ #   if sentence != '':
+  #      first_char = sentence[0]
+   # else:
+#    first_char = None
+    #return (len(sentence), first_char)
