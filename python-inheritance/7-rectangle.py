6b5808c814bcf080510206f02b261be13775d7b0
MuminaB <barrowmuminatou@gmail.com>
2023-12-14 23:46:06 +0000

    test

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,5 +1,5 @@
 """Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
