45f7adbffb26f87f8b1ac4f0ee5722d6ed9790ad
MuminaB <barrowmuminatou@gmail.com>
2023-12-14 23:17:15 +0000

    base

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,7 +1,7 @@
 """Code to extend BaseGeometry with Rectangle class"""
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
