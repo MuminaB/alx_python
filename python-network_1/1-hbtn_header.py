9040d5318de195c01204eccba40680aef6b4e3d5
MuminaB <barrowmuminatou@gmail.com>
2023-12-18 23:29:54 +0000

    update

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+"""Get value of `X-Request-Id` header from requested URL"""
+
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    response = get(url)
+    info = response.headers
+    print(info.get('x-request-id'))
---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,14 @@
+"""Take in URL and print content or status code on error"""
+
+from requests import get
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    response = get(url)
+    if response.status_code < 400:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
+         
