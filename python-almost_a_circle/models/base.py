e31c7c2b5206885d32f3ca5414168ca66e778d94
MuminaB <barrowmuminatou@gmail.com>
2023-12-18 20:51:52 +0000

    First class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+class Base:
+    """Base class to be subclassed by Square and Rectangle"""
+    __nb_object = 0
+    """Class variable representing the total count of Base (and subclass)
+    instances.
+    """
+
+    def __init__(self, id=None):
+        """Initialize new Base instance
+
+        Args:
+            id: Identifier for instance. If None, use current object count.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_object += 1
+            self.id = Base.__nb_object
+             
