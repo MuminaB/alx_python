ed4b61c12062da6d7b40a89d4798507b33200191
MuminaB <barrowmuminatou@gmail.com>
2024-01-14 01:04:59 +0000

    select states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+"""
+    Script that lists all states from a selected database
+    Takes three arguments: username, password and database name
+"""
+import MySQLdb
+database = MySQLdb.connect(user = "mumi", passwd = "password", db = "hbtn_0e_0_usa")
+cursor = database.cursor()
+cursor.execute("SELECT * FROM `states`")
+#[print(state) for state in cursor.fetchall()]
+results = cursor.fetchall
+for result in results:
+    print(result)
+     
