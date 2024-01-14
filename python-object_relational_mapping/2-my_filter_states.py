f871e106429a7f26dbe4bf12d150f6b4f695e3da
MuminaB <barrowmuminatou@gmail.com>
2024-01-14 18:24:54 +0000

    commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,14 +3,12 @@
 """
 import sys
 import MySQLdb
+if __name__ == "__main__":
+    db = MySQLdb.connect(name=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE BINARY name = '{}'".format(sys.argv[4]))
 
-db = MySQLdb.connect(name=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-c = db.cursor()
-c.execute("SELECT * \
-          FROM states \
-          WHERE BINARY name = '{}'".format(sys.argv[4]))
-
-states = c.fetchall()
-for state in states:
-    print(state)
+    states = c.fetchall()
+    for state in states:
+        print(state)
      
--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,6 +4,7 @@
 """
 import sys
 import MySQLdb
-db = MySQLdb.connect(name=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-c = db.cursor()
-c.execute("")
+if __name__ == "__main__":
+    db = MySQLdb.connect(name=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("")
