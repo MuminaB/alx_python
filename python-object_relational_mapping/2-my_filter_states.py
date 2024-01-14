49373139e3dc69902c5b05392b7d40518b0e8274
MuminaB <barrowmuminatou@gmail.com>
2024-01-14 18:44:11 +0000

    f

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,7 +3,7 @@
 """
 import sys
 import MySQLdb
-# if __name__ == "__main__":
+if __name__ == "__main__":
 #     db = MySQLdb.connect(name=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 #     c = db.cursor()
 #     c.execute("SELECT * FROM `states` WHERE BINARY `name` = '{}'".format(sys.argv[4]))
@@ -11,7 +11,7 @@ import MySQLdb
 #     states = c.fetchall()
 #     for state in states:
 #         print(state)
-if __name__ == "__main__":
+#if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * \
