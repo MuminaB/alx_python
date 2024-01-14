a1f1f7cf26ae74bf9670b9b8895be41c7f0820e5
MuminaB <barrowmuminatou@gmail.com>
2024-01-14 01:11:56 +0000

    d

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
     Takes three arguments: username, password and database name
 """
 import MySQLdb
-database = MySQLdb.connect(user = "root", passwd = "password123", db = "hbtn_0e_0_usa")
+database = MySQLdb.connect(user = "root", passwd = "password123", db = "hbtn_0d_0_usa")
 cursor = database.cursor()
 cursor.execute("SELECT * FROM `states`")
 #[print(state) for state in cursor.fetchall()]
