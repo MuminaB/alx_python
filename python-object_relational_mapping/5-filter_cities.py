141545b02dde8e34f50d91d0b6f28a6047eb1ae5
MuminaB <barrowmuminatou@gmail.com>
2024-01-16 20:06:09 +0000

    filter cities

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+"""
+    Script that takes in the name of a state as an argument and lists all cities
+    of that state.Script is injection free and takes in 4 arguments...
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states \
+              ON cities.states_id = states.id WHERE states.name = '{};" .format(sys.argv[4]))
+    
+    states = c.fetchall()
+    for state in states:
+        print(", ".join(state[1]))
+          
