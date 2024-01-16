1673e4b593e456981c162e5c5f42e194e4d1b8d9
MuminaB <barrowmuminatou@gmail.com>
2024-01-16 20:21:29 +0000

    format

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states \
-              ON cities.state_id = states.id WHERE states.name = '{};" .format(sys.argv[4]))
+              ON cities.state_id = states.id WHERE states.name = '{}';" .format(sys.argv[4]))
     
     states = c.fetchall()
     for state in states:
-        print(", ".join([state[1]]))
-          
+        print(", ".join(state[1]))
+            
