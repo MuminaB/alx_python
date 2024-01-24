85df6cf9f18023a094294abf467f8991f8624b3f
MuminaB <barrowmuminatou@gmail.com>
2024-01-24 19:00:38 +0000

    commit

---
+++ python-web_framework/6-number_odd_or_even.py
@@ -0,0 +1,45 @@
+"""
+Script that starts a flask web application
+"""
+
+from flask import Flask, render_template
+
+"""An instance of Flask"""
+app = Flask(__name__)
+
+"""route for '/' """
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+"""Route for '/hbnb'"""
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+"""route for '/c/<text>'"""
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    text = text.replace("_", " ")
+    return "C " + text
+
+"""route for '/python/<text>'"""
+"""url with a text parameter"""
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False) 
+@app.route("/python/<text>", strict_slashes=False) #url without any text parameter
+def python(text):
+    text = text.replace("_", " ")
+    return "Python " + text
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+
+"""run the app on required ip and port"""
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
