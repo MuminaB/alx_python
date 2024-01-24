2a028f76df3ba13497a8f80f806aef01816b869c
MuminaB <barrowmuminatou@gmail.com>
2024-01-24 02:02:10 +0000

    hbnb

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,22 @@
+"""
+Script that starts a flask web application
+"""
+
+from flask import Flask
+
+"""An instance of Flask"""
+app = Flask(__name__)
+
+"""route for '/' """
+@app.route("/", strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+"""Route for '/hbnb'"""
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+"""run the app on required ip and port"""
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
