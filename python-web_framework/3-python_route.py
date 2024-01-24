562ace52eb7ff08da58b82f0aecf2d8194bdb208
MuminaB <barrowmuminatou@gmail.com>
2024-01-24 03:09:20 +0000

    text:is cool

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -24,7 +24,7 @@ def c(text):
     return "C " + text
 
 """route for '/python/<text>'"""
-@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/<text>", defaults=("text":"is cool") strict_slashes=False)
 def python(text):
     text = text.replace("_", " ")
     return "Python " + text
