614dc6b06fdbbde2f4ec16486a1cb77f2699f519
MuminaB <barrowmuminatou@gmail.com>
2024-02-26 17:18:10 +0000

    docstring

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -2,9 +2,25 @@ import json
 import requests
 import sys
 
+<<<<<<< HEAD
 def export_employee_todo_to_json(employee_id):
     """
     Retrieves todo list progress for a given employee ID using the REST API.
+=======
+def get_employee_todo_progress(employee_id):
+    """
+    Retrieves TODO list progress for a given employee ID using the REST API.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        None: Displays the employee's TODO list progress in the specified format.
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_endpoint = f"{base_url}/users/{employee_id}"
+    todos_endpoint = f"{base_url}/users/{employee_id}/todos"
+>>>>>>> 965849c (docstring)
 
     Args:
         employee_id (int): The ID of the employee.
