db876b7bbce37a2c73b30644b15911a3b603f7b6
MuminaB <barrowmuminatou@gmail.com>
2024-02-27 22:03:20 +0000

    k

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,14 +1,18 @@
 import csv
 import requests
 <<<<<<< HEAD
+<<<<<<< HEAD
 import sys
 
 def export_employee_todo_to_csv(employee_id):
     """
     Retrieves TODO list progress for a given employee ID using the REST API.
 =======
+=======
+import sys
+>>>>>>> 2321a51 (k)
 
-def get_employee_todo_progress(employee_id):
+def export_employee_todo_to_csv(employee_id):
     """
     Retrieves TODO list progress for a given employee ID using the REST API.
 
@@ -18,6 +22,7 @@ def get_employee_todo_progress(employee_id):
     Returns:
         None: Displays the employee's TODO list progress in the specified format.
     """
+<<<<<<< HEAD
     base_url = "https://jsonplaceholder.typicode.com"
     user_endpoint = f"{base_url}/users/{employee_id}"
     todos_endpoint = f"{base_url}/users/{employee_id}/todos"
@@ -45,6 +50,21 @@ def get_employee_todo_progress(employee_id):
         print(f"{csv_filename} created successfully!")
 >>>>>>> 0548cc1 (try)
 
+=======
+    # Fetch employee details
+    employee_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    employee = employee_response.json()
+
+    # Fetch employee's TODOs
+    todos_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
+    todos = todos_response.json()
+
+    # Prepare data for CSV
+    data = []
+    for todo in todos:
+        data.append([employee_id, employee["name"], todo['completed'], todo['title']])
+
+>>>>>>> 2321a51 (k)
     # Write data to CSV
     with open(f'{employee_id}.csv', 'w', newline='') as file:
         writer = csv.writer(file, quoting=csv.QUOTE_ALL)
@@ -52,4 +72,7 @@ def get_employee_todo_progress(employee_id):
 
 if __name__ == "__main__":
     export_employee_todo_to_csv(int(sys.argv[1]))
+<<<<<<< HEAD
  
+=======
+>>>>>>> 2321a51 (k)
